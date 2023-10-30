d59232295976ad58ab152bc4de72cfdc1d5d606a
LegendArmour <35869955+LegendArmour@users.noreply.github.com>
2023-10-30 15:32:09 +0000

    ETAHEN

---
+++ payload_map.js
@@ -0,0 +1,19 @@
+const payload_map =
+    [
+        {
+            displayTitle: 'etaHEN without cheats',
+            description: 'AIO payload',
+            fileName: 'etaHEN.bin',
+            author: 'LM, illusion0001, astrelsky, sleirsgoevy',
+            source: 'https://github.com/LightningMods/etaHEN',
+            version: '1.0b'
+        },
+        {
+            displayTitle: 'etaHAN with cheats',
+            description: 'AIO payload',
+            fileName: 'etaHENwithcheats.bin',
+            author: 'illusion0001, astrelsky',
+            source: 'https://github.com/LightningMods/etaHEN',
+            version: '1.0b'
+        }
+    ];
